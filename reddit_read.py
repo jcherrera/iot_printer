a2baa538477abe818b728dc9a5da1bc67c7d9bc4
jcherrera <j.c.h@me.com>
2015-05-27 10:35:54 -0600

    second commit

---
+++ reddit_read.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+# Read 5 (hot) posts from Reddit /r/news and send to printer
+# Juan Carlos Herrera
+
+from __future__ import print_function
+__author__ = 'Juan Carlos Herrera <j.c.h@me.com>'
+__source__ = 'https://github.com/jcherrera'
+
+import re
+import praw
+import urllib, time
+from Adafruit_Thermal import *
+
+
+# Initialize the Printer
+printer     = Adafruit_Thermal("/dev/ttyAMA0", 19200, timeout=5)
+
+#Read Reddit
+user_agent = ("pixo")
+
+r = praw.Reddit(user_agent = user_agent)
+
+subreddit = r.get_subreddit("news")
+
+for submission in subreddit.get_hot(limit = 5):
+    printer.print('Title:' + submission.title )
+    printer.print('Score: ' + submission.score)
+    printer.print ('------------------------------ \n')
